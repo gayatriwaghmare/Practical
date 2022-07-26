804080d0bf78fe1a9146d6e4c0dc9c6ab2799c58
Gayatri Waghmare <62735216+gayatriwaghmare@users.noreply.github.com>
2022-07-26 12:26:05 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,7 @@
+#input={'ID':{1,2,3},'Name':{'XYZ','ABC','PQR'},'Address':{'Pune','Baner','Mumbai'}}
+#output=[{'ID': 1, 'Name': 'XYZ', 'Address': 'Mumbai'}, {'ID': 2, 'Name': 'PQR', 'Address': 'Baner'}, {'ID': 3, 'Name': 'ABC', 'Address': 'Pune'}]
+
+
 input={'ID':{1,2,3},'Name':{'XYZ','ABC','PQR'},'Address':{'Pune','Baner','Mumbai'}}
 l=[]
 for i in input:
